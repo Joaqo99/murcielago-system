fb4f2ea421139db49b74edc1165a0147547bbc57
Joaqo <joaqo9909@gmail.com>
2025-01-24 20:52:31 -0300

    Primer versión de CNN terminada

--- cnn.py
+++ cnn.py
@@ -44,6 +44,64 @@ class ShotDetectionNetwork(nn.Module):
         y = self.conv2(y)
         logits = self.classifier(y)
         return logits
+    
+
+
+class ShotDetectionNetwork2(nn.Module):
+    def __init__(self, hidden_units, num_layers):
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=1,
+                out_channels = hidden_units,
+                kernel_size = 4,
+                padding = 1,
+                stride = 2
+            ),
+
+            nn.Conv2d(
+                in_channels=hidden_units,
+                out_channels = int(hidden_units/4),
+                kernel_size = 4,
+                padding = 1,
+                stride = 2
+            ),
+            nn.MaxPool2d(kernel_size=2),
+        )
+
+        self.additional_layers = nn.ModuleList([
+            nn.Conv2d(
+                in_channels=int(hidden_units / 4),
+                out_channels=int(hidden_units / 4),
+                kernel_size=3,
+                padding=1,
+                stride=1
+            ) for _ in range(num_layers)
+        ])
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels= int(hidden_units/4),
+                out_channels = 1,
+                kernel_size = 2,
+                padding = 1,
+                stride = 2
+            ),
+            nn.MaxPool2d(kernel_size=2),
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.LazyLinear(out_features=1),
+        )
+    
+    def forward(self, x):
+        y = self.conv1(x)
+        for layer in self.additional_layers:
+            y = layer(y)
+        y = self.conv2(y)
+        logits = self.classifier(y)
+        return logits
 
 
 if __name__ == "__main__":
--- train_test_functions.py
+++ train_test_functions.py
@@ -5,9 +5,9 @@ from custom_dataset import GunShotsNoisesDataset, split_dataset
 import numpy as np
 
 
-def train_step(model, data_loader, loss_fn, optimizer, accuracy_fn, device="cpu"):
+def train_step(model, data_loader, loss_fn, optimizer, metric, device="cpu"):
     """Performs a training with model trying to learn on data_loader"""
-    train_loss, train_acc = 0, 0
+    train_loss = 0
 
     model.to(device)
 
@@ -18,18 +18,21 @@ def train_step(model, data_loader, loss_fn, optimizer, accuracy_fn, device="cpu"
 
         loss = loss_fn(y_logits, y)
         train_loss += loss
-        train_acc += accuracy_fn(y_true=y, y_logits=y_logits)
+        metric.update(y_logits, y)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
     train_loss /= len(data_loader)
-    train_acc /= len(data_loader)
+    train_acc = metric.compute()*100
 
-    print(f"Train loss: {train_loss:.5f} | Train accuracy: {train_acc:.5f}%")
+    print(f"Train loss: {train_loss:.5f} | Train accuracy: {train_acc}%")
+
+    metric.reset()
+    return train_loss
 
 
-def test_step(model, data_loader, loss_fn, accuracy_fn, device="cpu"):
+def test_step(model, data_loader, loss_fn, metric, device="cpu"):
     """Performs a training with model trying to learn on data_loader"""
     test_loss, test_acc = 0, 0
 
@@ -39,15 +42,16 @@ def test_step(model, data_loader, loss_fn, accuracy_fn, device="cpu"):
         for X, y in data_loader:
             X, y = X.to(device), y.to(device)
 
-            test_pred = model(X).squeeze()
-            
-            test_loss += loss_fn(test_pred, y)
-            test_acc += accuracy_fn(y_true=y, y_logits=test_pred)
+            test_logits = model(X).squeeze()
+            test_pred = torch.round(torch.sigmoid(test_logits))
+            print(f"Predicción: {test_pred} | Respuesta: {y}")
+            test_loss += loss_fn(test_logits, y)
+            metric.update(test_logits, y)
 
         test_loss /= len(data_loader)
-        test_acc /= len(data_loader)
+        test_acc = metric.compute()*100
 
-        print(f"Test loss: {test_loss:.5f} | Test accuracy: {test_acc:.5f}%")
+        print(f"Test loss: {test_loss:.5f} | Test accuracy: {test_acc}%")
 
 def eval_model(model: torch.nn.Module, data_loader, loss_fn, accuracy_fn):
     """
